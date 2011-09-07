42e73eeb15b9f349ae5b03ed5f3cabc4d462b2f5
Tim Kelty <timkelty@gmail.com>
2011-09-07 13:51:04 -0400

    fixing sshkey

--- plugins/available/ssh.plugins.bash
+++ plugins/available/ssh.plugins.bash
@@ -9,19 +9,19 @@ function sshlist() {
 }
 
 # export ssh public key to a remote server
-function sshkey {
+function sshkey() {
   KEY="~/.ssh/id_dsa.pub"
 
   if [ ! -f ~/.ssh/id_dsa.pub ];then
       echo "private key not found at $KEY"
       echo "* please create it with "ssh-keygen -t dsa" *"
       echo "* to login to the remote host without a password, don't give the key you create with ssh-keygen a password! *"
-      exit
+      return
   fi
 
   if [ -z $1 ];then
       echo "Please specify user@host.tld as the first switch to this script"
-      exit
+      return
   fi
 
   echo "Putting your key on $1... "
